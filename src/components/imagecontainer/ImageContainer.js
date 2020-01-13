3b8384e2175407832fdf79910e02e5bfcf621d95
saulperez37 <sperez_44@hotmail.com>
2020-01-12 21:33:30 -0700

    created react app, added folders and files

---
+++ src/components/imagecontainer/ImageContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImageContainder = () => {
+    return (
+        <ImageContainder />
+    );
+};
+
+export default ImageContainder;
---
+++ src/components/scorecounter/ScoreCounter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ScoreCounter = () => {
+    return (
+        <ScoreCounter />
+    );
+};
+
+export default ScoreCounter;
